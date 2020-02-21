446b6eb6bce552023c11bde1fcf04cad6dc58b7a
Lucas Deleforge <deleforge.lucas@gmail.com>
2020-02-21 21:05:35 +0100

    chore(main): init main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+    // If there are arguments, use command line
+    if(argc > 1){
+        cout << "coucou";
+    }
+    // Else run the GUI creator
+
+}
