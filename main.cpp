26f439edf1eaabff5fb1583e2aff155397eec182
Lucas Deleforge <deleforge.lucas@gmail.com>
2020-02-22 20:40:56 +0100

    feat: add file flux to write class code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "MetaClass.h"
+#include <fstream>
 
 using namespace std;
 
 
 int main(int argc, char *argv[]){
     // If there are arguments, use command line
+    ofstream headerFlux("./test.h");
     if(argc > 1){
         MetaClass c1(argv[1]);
-        cout << c1;
+        headerFlux << c1;
     }
     // Else run the GUI creator
 
